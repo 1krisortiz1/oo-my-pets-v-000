61f7b7532005187da1a2db4b2734d6ce0bedb412
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-19 05:20:35 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,18 +1,15 @@
 class Owner
   attr_accessor :name, :pets
   attr_reader :species
+  @@owners = []
 
- @@owners = []
   def initialize(species)
     @species = species
-
-  @@owners << self
-
-
+    @@owners << self
   end
 
   def say_species
     "I am a #{@species}."
   end
-  end
+
 end
